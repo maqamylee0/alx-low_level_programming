3a71b996c71a4d16b7692cc2fbec899e4fc5c09f
maqamylee0 <maqamylee0@gmail.com>
2023-02-16 12:09:49 -0800

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - print text without prinf
+ * Return: 1 on sucess
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
