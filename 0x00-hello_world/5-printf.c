f1536213fbb704a7a7ab9805fae7917ffb656021
maqamylee0 <maqamylee0@gmail.com>
2023-02-16 10:50:58 -0800

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -prints text
+ * 
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
