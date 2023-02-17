1f187af055057f3d7e7d009d20c7f7d9c0d60466
maqamylee0 <maqamylee0@gmail.com>
2023-02-17 12:01:39 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main -main function
+ * @void: empty params
+ * Description: Prints all hexadecimals
+ * Return: zero  for success
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+ 	}
+	putchar('\n');
+	return (0);
+}
