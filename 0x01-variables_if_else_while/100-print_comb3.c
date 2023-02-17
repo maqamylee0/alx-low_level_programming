c172da31f7cde0192319a676722a25426e998057
maqamylee0 <maqamylee0@gmail.com>
2023-02-17 12:36:13 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @void: Empty params.
+ * Description:prrints possible comb of 2 numbers separated by a ,
+ * Return: zero for success
+*/
+int main(void)
+{
+}
