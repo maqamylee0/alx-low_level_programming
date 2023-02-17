6fb9d2d03e3a26ad771446e7fa1f051dc4b62370
maqamylee0 <maqamylee0@gmail.com>
2023-02-17 12:32:56 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ * @void: Empty params
+ * Description: prints combination of 3 digitser 00-99
+ * Return: zero  for success
+*/
+int main(void)
+{
+}
