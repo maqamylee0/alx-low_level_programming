f9fbc1edba3b8fa01b715828cb9d7db6b88db837
maqamylee0 <maqamylee0@gmail.com>
2023-02-17 11:49:50 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print alphabet in reverse
+ * @void: empty params
+ * Description:prints alphabet in revers
+ * Return:  zero for success
+*/
+int main(void)
+{
+	int n = 57;
+
+	while (n <= 48)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
