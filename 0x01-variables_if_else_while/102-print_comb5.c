17a776b46d8974ff8667088052426fcc874d434e
maqamylee0 <maqamylee0@gmail.com>
2023-02-17 12:30:32 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ * @void: Empty parameter list for main.
+ * Description:prints combinations of numbers
+ * Return: zero for success
+*/
+int main(void)
+{
+
+}
