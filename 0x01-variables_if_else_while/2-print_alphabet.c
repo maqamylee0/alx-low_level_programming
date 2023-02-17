04ddc9c6d8a470268cceaef9efac399a990c877d
maqamylee0 <maqamylee0@gmail.com>
2023-02-17 10:23:01 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main = main function
+ * Return: zero or success
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar("\n");
+}
