2ef5e0215f3f2736989ba39814e3fd2b028079cb
maqamylee0 <maqamylee0@gmail.com>
2023-02-17 10:51:24 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet
+ * @void: Empty parameter list for main.
+ * Description: Prints the alphabet
+ * Return: zero or success
+*/
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
