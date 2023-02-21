7d2f1248743b3fd2f8d6dc93affdf6d100d0586b
maqamylee0 <maqamylee0@gmail.com>
2023-02-21 09:17:36 +0300

    work

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - main function
+ * print_alphabet(void): method to print alphabet
+ * @void: empty params
+ * Description: print alphabet
+ * Return: zero or success
+ */
+void print_alphabet(void)
+{
+    int i;
+
+    for (i = 97; i <= 122; i++)
+    {
+    putchar(i);
+    }
+}
+int main(void)
+{
+    print_alphabet();
+    putchar('\n');
+    return (0);
+}
