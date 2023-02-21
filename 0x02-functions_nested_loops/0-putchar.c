c01ca42f2143420abc1bc1451fa2e1f3f1987d29
maqamylee0 <maqamylee0@gmail.com>
2023-02-21 08:37:42 +0300

    work

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: zero or success
+ */
+int main(void)
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+	
+	return (0);
+}
