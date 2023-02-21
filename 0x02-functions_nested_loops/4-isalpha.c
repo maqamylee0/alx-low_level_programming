6993c107dc64a00dd7fec3e65392ced6cc9b00cb
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 00:14:54 +0300

    ji

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * _isalpha - is upper or lower case
+ * @void: empty params
+ * Description: check uppr or lower case
+ * Return: zero or success
+ */
+
+int _isalpha(int c)
+{
+
+if (c <= 122 && c >= 97 || c >= 65 && c <= 121)
+		return (1);
+	else
+		return (0);
+}
