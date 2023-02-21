aafbb59c483e522eeb57adf4c1212d8435f526a6
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 00:08:56 +0300

    ji

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,13 +6,13 @@
  * _islower - method to check lower case
  * @c: character to check
  * Description: print alphabet
- * Return:  1 if lower case, 0 if not
+ * Return:  1 if lower case, 0 if 
  */
 
 int _islower(int c)
 {
-if (c <= 122 && c >= 97)
-return (1);
-else
-return (0);
+	if (c <= 122 && c >= 97)
+		return (1);
+	else
+		return (0);
 }
