0e390758d6ffb8cf3e35c0aaee951c321511ffba
maqamylee0 <maqamylee0@gmail.com>
2023-02-21 21:59:53 +0300

    work

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,23 +4,26 @@
 #include <stdio.h>
 /**
  * main - main function
- * print_alphabet(void) - method to print alphabet
+ * function print_alphabet_x10 - method to print alphabet
  * @void: empty params
  * Description: print alphabet
  * Return: zero or success
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-int i;
-
+int i, j = 1;
+while (j <= 10)
+{
 for (i = 97; i <= 122; i++)
 {
 putchar(i);
 }
+putchar('\n');
+j++;
+}
 }
 int main(void)
 {
-print_alphabet();
-putchar('\n');
+print_alphabet_x10();
 return (0);
 }
