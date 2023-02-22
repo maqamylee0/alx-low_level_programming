d82ba48f3b17359be4fcc3cef7f99c78b53e16ca
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 17:31:45 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- main function
+ * @void :  empty  string
+ * Description: print natural numbers
+ * Return: zero 0r 1
+ */
+
+int main(void)
+{
+	
+	
+	return (0);
+}
+
+
