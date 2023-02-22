c5043c5a0c3bca53818b1d28870fbf636f4f5d7b
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 12:25:08 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@
 
 void print_alphabet_x10(void)
 {
-    int i, j = 1;
-    while (j <= 10)
-    {
-        for (i = 97; i <= 122; i++)
-        {
+int i, j = 1;
+while (j <= 10)
+{
+for (i = 97; i <= 122; i++)
+{
             _putchar(i);
         }
         _putchar('\n');
