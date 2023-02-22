58bd616ce195471af4d02e8032e0623ae81da3e3
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 13:02:22 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,6 +8,7 @@
 void print_alphabet_x10(void)
 {
 int i, j = 1;
+
 while (j <= 10)
 {
 for (i = 97; i <= 122; i++)
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * main - main function
- * _islower - method to check lower case
- * @c: character to check
- * Description: print alphabet
- * Return:  1 if lower case, 0 if 
+ *_islower - method to check lower case
+ *@c : integer param
+ *Description: print alphabet
+ *Return:  1 if lower case, 0 if
  */
-
 int _islower(int c)
 {
-	if (c <= 122 && c >= 97)
-		return (1);
-	else
-		return (0);
+if (c <= 122 && c >= 97)
+return (1);
+else
+return (0);
 }
