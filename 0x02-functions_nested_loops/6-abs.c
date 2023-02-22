be81a3a6459896049600407eb496565031d0a13e
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 13:23:27 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - is upper or lower case
+ * @a : integer input
+ * Description: check uppr or lower case
+ * Return: zero 0r 1
+ */
+
+int _abs(int a)
+{
+
+return (a*a)/a;
+
+}
