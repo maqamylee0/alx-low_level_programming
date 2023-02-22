3b005f315b43ed2527a00b4dedeb04a9cf421714
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 13:33:07 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - is upper or lower case
+ * @a : integer input
+ * Description: check uppr or lower case
+ * Return: zero 0r 1
+ */
+
+int print_last_digit(int a)
+{
+return (a % 10);
+}
