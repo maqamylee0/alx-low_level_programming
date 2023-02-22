1da0ab8dda380a38af9fe5abe419587328fe8cb1
maqamylee0 <maqamylee0@gmail.com>
2023-02-22 15:20:47 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ for (a = 0; a <= 24; a++)
 
 for (b = 0; b <= 60; b++)
 {
-_putchar('0' + a / 10);
-_putchar('0' + a % 10);
+_putchar('0' + (a / 10));
+_putchar('0' + (a % 10));
 _putchar(':');
-_putchar('0' + b / 10);
-_putchar('0' + b % 10);
+_putchar('0' + (b / 10));
+_putchar('0' + (b % 10));
 _putchar('\n');
 
 }
