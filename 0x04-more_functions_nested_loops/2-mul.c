8502ff2b9e7257a61f6490b6693397a9cca0afa8
maqamylee0 <maqamylee0@gmail.com>
2023-02-23 22:42:32 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ * @a: first int input
+ * @b: second int input
+ * Return: product of both ints
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,3 @@
 int _isupper(int);
 int _isdigit(int c);
-
+int mul(int a, int b);
