5876eccbaeec2e2d3be9a831bcb01c2860619509
maqamylee0 <maqamylee0@gmail.com>
2023-02-24 01:56:08 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the number from 0-14
+ * Return: nothing
+ **/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar(i % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
+
