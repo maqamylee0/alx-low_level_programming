acce78195921175ffd2bfbbb455af8acfba2640b
maqamylee0 <maqamylee0@gmail.com>
2023-02-28 12:01:19 -0800

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * 1-swap: awsps values of 2 integers
+ * @int *a-pointer 1
+ * @int *b-pointer 
+ * Description:swap numbers
+ * Return-empty
+ */
+
+void swap_int(int *a, int *b)
+{
+	int z = *a;
+	*a = *b;
+	*b = c;
+}
