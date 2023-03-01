9649005d1028561618a9fd93b5228870e954e361
maqamylee0 <maqamylee0@gmail.com>
2023-02-28 18:41:03 -0800

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: input string
+ * Description: reverses the string
+ * Return: nothing
+ **/
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	int z = 0;
+
+	char rev[1000];
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	
+	for (; i > 0; i--)
+	{
+		rev[z] = s[i - 1];
+		z++;
+	}
+	
+	while (i < j)
+	{
+		s[i] = rev[i];
+		i++;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void _putchar(char);
+void rev_string(char *s);
