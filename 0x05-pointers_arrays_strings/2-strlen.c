7b097eb957bb156107a3ed35f444a82871c1a935
maqamylee0 <maqamylee0@gmail.com>
2023-02-28 17:52:58 -0800

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - find lenght of string
+ * @s : parameter one
+ * Description:function counts lenght of a string
+ * Returns: integer for lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	
+	while(*s != "\0")
+	{
+		len++;
+	}
+	return len;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts -a string, followed by a new line
+ * @*s :input pointer to string
+ * Description: a string, followed by a new line
+ * Returns:nothing
+ */
+
+void _puts(char *str)
+{
+	while(*s != "\0")
+	{
+		_putchar(*s);
+	}
+	_putchar(*s);
+}
