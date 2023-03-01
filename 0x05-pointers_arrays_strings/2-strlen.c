cf4306fc2862ced4cf795b55c1991df8bee72b51
maqamylee0 <maqamylee0@gmail.com>
2023-02-28 18:14:10 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,13 @@
  * _strlen- find lenght of string
  * @s : parameter one
  * Description: function counts lenght of a string
- * Returns: integer for lenght of string
+ * Return: integer for lenght of string
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
+	
 	while (s[len] != '\0')
 	{
 		len++;
