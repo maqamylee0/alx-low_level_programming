5286cf59677b01734a13874b9cf5f95fda1fb91e
maqamylee0 <maqamylee0@gmail.com>
2023-03-03 04:17:09 -0800

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,39 +9,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	int j = 0;
+	i = 0;
 
-	int z = 0;
-
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
 
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-
-	if (i > j)
-	{
-		return (1);
-	}
-	else
-		if (i < j)
-		{
-			return (-1);
-		}
-		else
-		{
-			for (; s1[z] != '\0'; z++)
-			{
-				if (s1[z] != s2[z])
-					break;
-			}
-			return (0);
-		}
+	if (s1[i] == s2[i])
+		return (0);
+	return (s1[i] - s2[i]);
 }
 
