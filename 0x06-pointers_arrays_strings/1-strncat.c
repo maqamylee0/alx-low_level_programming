f49e60abdf0867deb21057a114ca9cca78236aaf
maqamylee0 <maqamylee0@gmail.com>
2023-03-03 00:49:32 -0800

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concat two string with n from second string
+ * @dest: first string
+ * @src: second string
+ * @n: number of characters to copy from second string
+ * Description: concat two string
+ * Return: pointer to result string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
+}
+
