dedcac4e82e821878452645769f1b9683cfd10c1
maqamylee0 <maqamylee0@gmail.com>
2023-03-03 06:53:11 -0800

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 - encoding with rot13
+ * @a: input string
+ * Description: encoding in rot13
+ * Return: string
+ */
+
+char *rot13(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if ((a[i] >= 'a' || a[i] >= 'A') && (a[i] <= 'z' || a[i] <= 'Z'))
+		{
+			a[i] = a[i] + 13;
+		}
+		i++;
+	}
+}
+
