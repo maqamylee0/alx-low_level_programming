83433950bf3f7c2c41c1cce0bf7639e2863154f9
maqamylee0 <maqamylee0@gmail.com>
2023-03-03 08:41:30 -0800

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers
+ * @n: input number
+ * Description: print numbers
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
