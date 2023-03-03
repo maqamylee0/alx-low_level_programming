8aa3edb7da15e061b3053a3c19a70a3e41b5d878
maqamylee0 <maqamylee0@gmail.com>
2023-03-03 07:23:42 -0800

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,11 +11,18 @@ char *rot13(char *a)
 {
 	int i = 0;
 
+	char *c[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+	char *e[] = {"abcdefghijklmnopqrstuvwxyz"}
+
 	while (a[i] != '\0')
 	{
-		if ((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z'))
+		if (((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z') ) && ((a[i] <= 'n') || (a[i] <= 'N')))
 		{
 			a[i] = a[i] + 13;
+		}
+		else
+		{
+			a[i] = a[i] - 13;
 		}
 		i++;
 	}
