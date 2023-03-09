daf65b079a21a4e607895fe01f530e10ef8eb65e
maqamylee0 <maqamylee0@gmail.com>
2023-03-09 04:31:49 -0800

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion = prints string
+ * _puts_recursion - prints string
  * @s: input string
  * Description: prints string
  * Return: nothing
@@ -9,10 +9,9 @@
 
 void _puts_recursion(char *s)
 {
-	
 	if (*s == '/0')
 		return;
 	_putchar(*s);
-	_puts_recursion(*(s+1));
+	_puts_recursion(*(s + 1));
 }
 
