4632354cbb1c716caaa8dd5c15208badbcd9b322
maqamylee0 <maqamylee0@gmail.com>
2023-03-09 06:24:07 -0800

    0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - lenght of string
+ * @s: string
+ * Description:lenght of string
+ * Return: int lenght
+ */
+
+int _strlen_recursion(char *s)
+{
+	int x = 1;
+
+	while (*s == '\0')
+		return (0);
+	if (*(s + 1) == '\0')
+		return (1);
+	else
+		return (x + _strlen_recursion(s + 1));
+}
+
