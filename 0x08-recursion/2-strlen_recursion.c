59359c40d6066e20352729f1daf24cc4021d2f3a
maqamylee0 <maqamylee0@gmail.com>
2023-03-09 06:38:07 -0800

    0x08-recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,13 +11,12 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-
 	if (*(s + 1) == '\0')
 	{
 		return (1);
 	}
 	else
 	{
-		return (_strlen_recursion(s+1) + 1);
+		return (_strlen_recursion(s + 1) + 1);
 	}
 }
