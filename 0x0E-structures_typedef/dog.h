7329d6d1a591f8fa142be5436f0d1d93237621c3
maqamylee0 <maqamylee0@gmail.com>
2023-03-21 07:32:46 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ **/
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+}dog_x;
