d9062803721f480f79a3df25a067b097fea1572d
maqamylee0 <maqamylee0@gmail.com>
2023-03-21 08:20:38 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -11,9 +11,9 @@ void free_dog(dog_t *d)
 	if (d == NULL)
 		return;
 	if (d->owner != NULL)
-		free(hobbes->owner);
+		free(d->owner);
 	if (d->name != NULL)
-		free(hobbes->name);
+		free(d->name);
 
 	free(d);
 }
