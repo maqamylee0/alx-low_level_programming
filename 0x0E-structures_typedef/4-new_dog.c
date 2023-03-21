1d642f63dbef97951151273ba26df3a89e5c0b1f
maqamylee0 <maqamylee0@gmail.com>
2023-03-21 08:06:25 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * new_dog - creates dog object
+ * @name: dog name
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog object, NULL if fails
+ **/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return NULL;
+	return (d);
+}
