4837ede321926cab47a6cb2dc92167afdd7ebd8f
maqamylee0 <maqamylee0@gmail.com>
2023-03-21 15:09:43 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,33 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d == NULL)
+	dog_t *y;
+
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	return (d);
+
+	y = malloc(sizeof(dog_t));
+
+	if (y == NULL)
+		return (NULL);
+
+	(*y).name = malloc(sizeof(char) * (strlen(name) + 1));
+	if ((*y).name == NULL)
+	{
+		free(y);
+		return (NULL);
+	}
+	(*y).owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if ((*y).owner == NULL)
+	{
+		free(y->name);
+		free(y);
+		return (NULL);
+	}
+
+	(*y).name = strcpy(y->name, name);
+	(*y).age = age;
+	(*y).owner = strcpy(y->owner, owner);
+
+	return (y);
 }
