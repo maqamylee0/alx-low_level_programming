370af81f52b4e4aebfbb45cafbead405b694ef3b
maqamylee0 <maqamylee0@gmail.com>
2023-03-23 13:11:02 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
 #ifndef __VAR_H_
 #define __VAR_H_
 
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
