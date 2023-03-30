953349e379433183fc362c2bd0fe8dda22505aac
maqamylee0 <maqamylee0@gmail.com>
2023-03-30 15:56:52 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,14 +14,14 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *node = malloc(sizeof(list_t));
 	int i = 0;
 
-	while (str[i] != '\0' && str != NULL)
+	if (node == NULL)
+		return (NULL);
+	if (str[i] == NULL)
+		return (0);
+	while (str[i] != '\0')
 	{
 		i++;
 	}
-
-	if (new == NULL)
-		return;
-
 	node->str = new;
 	node->len = i;
 	node->next = *head;
