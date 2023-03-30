7851b6841112e5c441a53225ac9c2dc43d859671
maqamylee0 <maqamylee0@gmail.com>
2023-03-30 16:29:00 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end- add node to end of list
+ * @head: double pointer to start
+ * @str: string
+ *
+ * Return: address to end of list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nextNode, *node;
+	int i = 0;
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	node->str = strdup(str);
+	node->len = i;
+	node->next = NULL;
+
+	newNode = *head;
+
+	if (newNode == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	while (*newNode->next != NULL)
+		newNode = newNode.next;
+	newNode->next = node;
+	return (node);
+}
