a73fb37fc5419595599001eb31ed5bff173c6644
maqamylee0 <maqamylee0@gmail.com>
2023-03-30 15:35:58 -0700

    first commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node- add node at start
+ * @head: pointer to pointer to start
+ * @str: string 
+ * 
+ * Return: address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *new = strdup(str);
+	list_t *node = malloc(sizeof(list_t));
+
+
+	if (new == NULL)
+		return;
+	node->str = new;
+	node->next = **head;
+
+	head->next = node;
+	free(new);
+}
