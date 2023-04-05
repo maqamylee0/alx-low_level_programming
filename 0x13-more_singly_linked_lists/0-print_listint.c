6a1db86e6eab2d036f90c71f83dbb38796fae151
maqamylee0 <maqamylee0@gmail.com>
2023-04-05 02:43:47 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	for (; h != NULL; count++)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
 	return count;
 }
