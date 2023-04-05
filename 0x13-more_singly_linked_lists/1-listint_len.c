4b85401c1e8d18665eb1a022d0f884da4294af1f
maqamylee0 <maqamylee0@gmail.com>
2023-04-05 06:12:51 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
 /**
  * listint_len- number of elements in a singlr linked list
  * @h: pointer to head of list
- * Return: number of elements in a linked list
+ * Return: number of elemrnts in a list
  */
 
 size_t listint_len(const listint_t *h)
@@ -11,6 +11,8 @@ size_t listint_len(const listint_t *h)
 	size_t count = 0;
 
 	for (; h != NULL; count++)
-		;
+	{
+		h = h->next;
+	}
 	return (count);
 }
