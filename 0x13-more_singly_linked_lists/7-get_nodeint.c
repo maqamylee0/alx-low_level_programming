0d85c64bbbc03f87b30f994c6f3688517242a58f
maqamylee0 <maqamylee0@gmail.com>
2023-04-06 05:11:15 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,8 +18,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (count == index)
 			return (now);
-		count++;
-		now->next = now;
+		if (count < index)
+		{
+			count++;
+			now->next = now;
+		}
 	}
 	return (NULL);
 }
