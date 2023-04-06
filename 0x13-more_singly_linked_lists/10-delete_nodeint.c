4d06b026343cb774c18a5a6c622bfb801f73a704
maqamylee0 <maqamylee0@gmail.com>
2023-04-06 09:23:52 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,6 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *next;
 
 	mover = (*head);
+	if (mover == NULL)
+		return (NULL);
 	while (mover != NULL)
 	{
 		next = mover->next;
@@ -24,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			free(mover);
 			return (1);
 		}
-		prev = mover;
+		prev = mover;i
 		count++;
 		mover = mover->next;
 	}
