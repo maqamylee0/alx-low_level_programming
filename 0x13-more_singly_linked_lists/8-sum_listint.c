14569073a39d95f9d4020e1e81b034e88907e2a6
maqamylee0 <maqamylee0@gmail.com>
2023-04-06 05:23:13 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint-sum of elements in a linked list
+ * head: pointer to head of list
+ * Return: sum of elements
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t now;
+
+	now = head;
+
+	while (now != NULL)
+	{
+		sum += now->n;
+		now = now->next;
+	}
+	return (sum);
+}
