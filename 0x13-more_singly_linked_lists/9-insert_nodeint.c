f619db01441fd20c1ee575f9941138c8119560e9
maqamylee0 <maqamylee0@gmail.com>
2023-04-06 07:34:54 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index- deletes node at index
+ * @head: double pointer to head
+ * @index: position to delete
+ * Return: 1 if successful -1 if fail
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *mover;
+	listint_t *prev;
+	listint_t *next;
+
+	mover = (*head);
+	while (mover != NULL)
+	{
+		prev = mover;
+		next = mover->next;
+		if (count == index)
+		{
+			prev->next = next;
+			return (1);
+		}
+		count++;
+		mover = movie->next;
+	}
+	return (-1);
+}
