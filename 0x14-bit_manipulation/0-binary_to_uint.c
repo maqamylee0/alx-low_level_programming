0ad364adad0b568ce986d594f1e622cc7ef73a8d
maqamylee0 <maqamylee0@gmail.com>
2023-04-10 13:16:58 -0700

    bit convert to binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * power- calculates power of 2
+ * @n: power of 2
+ * Return: result of powering 2
+ */
+
+unsigned int power(int n)
+{
+        unsigned int result = 1;
+
+        if (n == 0)
+                return (1);
+        result = 1 << n;
+        return (result);
+}
+/**
+ * binary_to_uint- converts binary to integer
+ * @b: character pointer to string of binary
+ * Return: unsigned int decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+        int count = 0, d = 0;
+        unsigned int number = 0, c;
+
+        if (b == NULL)
+                return (0);
+        while (b[d] != '\0')
+        {
+                if (b[d] !=  '0' && b[d] != '1')
+                        return (0);
+                d++;
+        }
+        while (d >= 0)
+        {
+                if (b[d] == '1')
+                        c = 1;
+                else
+                        c = 0;
+                number += c * power(count - 1);
+                count++;
+                d--;
+        }
+        return (number);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,18 @@
+#ifndef __BIT_H
+#define __BIT_H
+
+#include <stdlib.h>
+#include <string.h>
+
+char* cpy_str(const char *ptr, int len);
+char* reverse_string(char *ptr, int len);
+unsigned int power(int n);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _putchar(char c);
+#endif
