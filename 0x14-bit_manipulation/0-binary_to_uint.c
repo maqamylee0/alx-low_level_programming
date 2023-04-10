88acc021d0aab309d7ffb34045c7c22c5dabc6c5
maqamylee0 <maqamylee0@gmail.com>
2023-04-10 04:19:33 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int count = 0;
-	unsigned int number = 1, c;
+	unsigned int number = 0, c;
 
 	if (b == NULL)
 		return (0);
-	if (*b !=  "0" || *b != "1")
+	if (*b !=  '0' || *b != '1')
 		return (0);
 
-	while (*b != "\0")
+	while ((*b) != '\0')
 	{
-		if (*b == "1")
+		if ((*b) == '1')
 			c = 1;
 		else
 			c = 0;
-		number = c * power(count);
+		number += c * power(count);
 		b = b + 1;
 		count++;
 	}
+	return (number);
 }
