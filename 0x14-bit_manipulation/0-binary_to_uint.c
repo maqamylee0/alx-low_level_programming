f1ac218b53c52bccb794902c3243da3e2c2eb951
maqamylee0 <maqamylee0@gmail.com>
2023-04-10 13:24:33 -0700

    fix betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,12 +8,12 @@
 
 unsigned int power(int n)
 {
-        unsigned int result = 1;
+	unsigned int result = 1;
 
-        if (n == 0)
-                return (1);
-        result = 1 << n;
-        return (result);
+	if (n == 0)
+		return (1);
+	result = 1 << n;
+	return (result);
 }
 /**
  * binary_to_uint- converts binary to integer
@@ -23,26 +23,26 @@ unsigned int power(int n)
 
 unsigned int binary_to_uint(const char *b)
 {
-        int count = 0, d = 0;
-        unsigned int number = 0, c;
+	int count = 0, d = 0;
+	unsigned int number = 0, c;
 
-        if (b == NULL)
-                return (0);
-        while (b[d] != '\0')
-        {
-                if (b[d] !=  '0' && b[d] != '1')
-                        return (0);
-                d++;
-        }
-        while (d >= 0)
-        {
-                if (b[d] == '1')
-                        c = 1;
-                else
-                        c = 0;
-                number += c * power(count - 1);
-                count++;
-                d--;
-        }
-        return (number);
+	if (b == NULL)
+		return (0);
+	while (b[d] != '\0')
+	{
+		if (b[d] !=  '0' && b[d] != '1')
+			return (0);
+		d++;
+	}
+	while (d >= 0)
+	{
+		if (b[d] == '1')
+			c = 1;
+		else
+			c = 0;
+		number += c * power(count - 1);
+		count++;
+		d--;
+	}
+	return (number);
 }
