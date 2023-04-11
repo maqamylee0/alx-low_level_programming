08d639dce3a236f6b313f2c9140381f093ef603a
maqamylee0 <maqamylee0@gmail.com>
2023-04-11 10:15:38 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	{
 		if (index < (sizeof(unsigned long int) * 8))
 		{
-			*n |= 1 << index;
+			*n |= (1 << index);
 			return (1);
 		}
 		else
