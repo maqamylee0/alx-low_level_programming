d93d0125cb29a7cfa8bc5abb15c20545611a2d8b
maqamylee0 <maqamylee0@gmail.com>
2023-04-11 11:16:13 -0700

    fix betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,16 +4,20 @@
  * clear_bit- set bit to zero
  * @n: pointer to binary
  * @index: index
- * @Return:1 or -1
+ * Return:1 or -1
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n != NULL && index < (sizeof(unsigned long int) * 8))
+	if (n != NULL)
 	{
-		*n &= ~(1 << index);
-		
-		return (1);
+		if (index < (sizeof(unsigned long int) * 8))
+		{
+			*n &= ~(1 << index);
+			return (1);
+		}
+		else
+			return (-1);
 	}
 	else
 		return (-1);
