75cd3db1e9464e5ccfff2ce3796089dfda1aedbe
maqamylee0 <maqamylee0@gmail.com>
2023-04-10 17:38:56 -0700

    change method

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n != NULL)
+	if (n != NULL && index < (sizeof(unsigned long int) * 8))
 	{
-		*n &= 1 << index;
+		*n &= ~(1 << index);
 		
 		return (1);
 	}
