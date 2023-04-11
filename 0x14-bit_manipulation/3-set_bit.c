0b9f746a0b64135a9ec12ba330607d2a2aeea661
maqamylee0 <maqamylee0@gmail.com>
2023-04-10 17:22:10 -0700

    set bit value

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit- set bit at index
+ * @n: pointer to binary
+ * @index: index
+ * @Return: 1 0r -1 
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n != NULL)
+	{
+		n |= 1 << index;
+		return (1);
+	}
+	else
+		return (-1);
