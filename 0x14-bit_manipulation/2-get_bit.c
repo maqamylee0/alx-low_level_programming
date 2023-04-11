5f5b870ac252eb46c5ce031b84c4e579e82edee8
maqamylee0 <maqamylee0@gmail.com>
2023-04-11 07:43:33 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,12 +4,12 @@
  * get_bit- value at an index
  * @n: number
  * @index: index
- * @Return:value of bit
+ * Return: value of bit
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	n &= 1 << index;
 	if (n == 0)
