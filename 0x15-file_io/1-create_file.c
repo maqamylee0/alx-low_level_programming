22c5932feb409bf45d1f9e76c9ecb03d914e1fcf
maqamylee0 <maqamylee0@gmail.com>
2023-04-12 14:21:27 -0700

    rm extra bracket

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file- create a file
+ * @filename: name of file
+ * @text_content: content to write to text
+ * Return: 1 on success -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len;
+
+	if (filename == NULL)
+		return (-1);
+	if (access(filename, F_OK) != -1)
+		fd = open(filename, O_RDWR);
+	else
+		fd = open(filename, O_TRUNC);
+	if (fd == NULL)
+		return (-1);
+	while (*text_content != '\0')
+	{
+		len++;
+		text_content++;
+	}
+	if (text_content != NULL)
+	{
+		write(STDOUT_FILENO, *text_content, len);
+		return (1);
+	}
+	close(fd);
+}
