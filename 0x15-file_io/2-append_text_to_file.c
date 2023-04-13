c62895d84bb766a7988a51864809ee88a2a57b76
maqamylee0 <maqamylee0@gmail.com>
2023-04-13 14:52:00 -0700

    fix check filename

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,11 +13,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	int len = 0;
 	ssize_t w;
 
-	if (!filename == NULL)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if(fd == -1)
+	if (fd == -1)
 		return (-1);
 	while (*text_content != '\0')
 	{
