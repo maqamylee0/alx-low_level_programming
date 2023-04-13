1d1ab7d76cf264b9afe6a8e8c49442cf2642b97a
maqamylee0 <maqamylee0@gmail.com>
2023-04-13 07:14:32 -0700

    return bytes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	while ((i = read(fd, buffer, letters)) > 0)
 	{
 		write(STDOUT_FILENO, buffer, i);
-		read =+ i;
+		read += i;
 	}
 	free(buffer);
 	close(fd);
