086ab431785dc6e724fbacaad8492e2cda358517
maqamylee0 <maqamylee0@gmail.com>
2023-04-13 14:07:07 -0700

    add modes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,8 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 		write(STDOUT_FILENO, text_content, len);
+	else
+		write(STDOUT_FILENO, "", 0);
 	close(fd);
 	return (1);
 }
