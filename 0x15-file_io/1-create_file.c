e1c3d25cb553510716738a9e568e08e74e0f5925
maqamylee0 <maqamylee0@gmail.com>
2023-04-14 06:20:35 -0700

    check if getting length

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,6 +25,11 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
+		while (*text_content != '\0')
+		{
+			len++;
+			text_content++;
+		}
 		w = write(STDOUT_FILENO, text_content, len);
 		if (w == -1 || w != len)
 		{
