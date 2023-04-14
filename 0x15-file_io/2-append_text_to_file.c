eb4cbecd0ea0fe9fdd59c309d43016f8e7ba674a
maqamylee0 <maqamylee0@gmail.com>
2023-04-14 06:50:43 -0700

    change iteration

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,13 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
-		return (-1);
-	while (*text_content != '\0')
-	{
-		len++;
-		text_content++;
-	}
-
+		return (-1);	
 	if (text_content == NULL)
 	{
 		close(fd);
@@ -32,6 +26,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	else
 	{
+		while (text_content[len] != '\0')
+			len++;
 		w = write(fd, text_content, len);
 		if (w == -1 || w != len)
 		{
