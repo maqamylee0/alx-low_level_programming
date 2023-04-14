41717ded00c411b1d93670cecfae1deda0a7a326
maqamylee0 <maqamylee0@gmail.com>
2023-04-14 02:48:08 -0700

    check write bytes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	while ((i = read(fd, buffer, letters)) > 0)
 	{
 		w = write(STDOUT_FILENO, buffer, i);
-		if (w == -1)
+		if (w == -1 || w != i)
 		{
 			free(buffer);
 			return (0);
