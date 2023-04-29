f58e81b93f22113bed1d6a12e847985bbf1bce4b
maqamylee0 <maqamylee0@gmail.com>
2023-04-28 22:47:15 -0700

    use recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,20 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int holder;
-	int i, times = 0;
-
-	for (i = 63; i >= 0; i--)
-	{
-		holder = n >> i;
-		if (holder & 1)
-		{
-			times++;
-			_putchar('1');
-		}
-		if (times)
-			_putchar('0');
-	}
-	if (!times)
-		_putchar('0');
+	if (n == '0' || n == '1')
+		_putchar(n);
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
