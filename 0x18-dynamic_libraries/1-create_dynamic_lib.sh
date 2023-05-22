cce8604c2855cf90a575c496ec3037a0067cd79c
maqamylee0 <maqamylee0@gmail.com>
2023-05-22 15:52:01 -0700

    script to create lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -c dynamo.c dynamo2.c dynamo3.c dynamo4.c
-gcc -shared -o liball.so dynamo.c dynamo2.c dynamo3.c dynamo4.c
+gcc -shared -o liball.so dynamo.o dynamo2.o dynamo3.o dynamo4.o
