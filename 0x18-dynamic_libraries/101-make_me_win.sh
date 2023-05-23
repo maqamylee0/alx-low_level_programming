edcd97f21f204ff0d8bcea317c83dd789f79b093
emmilly immaculate <57020210+maqamylee0@users.noreply.github.com>
2023-05-23 03:33:23 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /usr  https://github.com/maqamylee0/alx-low_level_programming/raw/main/0x18-dynamic_libraries/rand.o
+wget -P /usr  https://github.com/maqamylee0/alx-low_level_programming/raw/main/0x18-dynamic_libraries/rand.so
 export LD_PRELOAD=/usr/sure_win.so
