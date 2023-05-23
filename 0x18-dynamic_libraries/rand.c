f2e7dcbcb224adb85dc7a7aff27d9064c0d342a9
maqamylee0 <maqamylee0@gmail.com>
2023-05-22 18:06:40 -0700

    add sh

--- 0x18-dynamic_libraries/rand.c
+++ 0x18-dynamic_libraries/rand.c
@@ -8,17 +8,23 @@ int rand()
 	switch (c)
 	{
 		case 1:
-			return 0;
+			return 8;
+			break;
 		case 2:
-			return 0;
+			return 8;
+			break;
 		case 3:
-			return 0;
+			return 7;
+			break;
 		case 4:
 			return 9;
+			break;
 		case 5:
 			return 23;
+			break;
 		case 6:
-			return (9 - 1);
+			return (74);
+			break;
 		default:
 			return 1;
 	}
