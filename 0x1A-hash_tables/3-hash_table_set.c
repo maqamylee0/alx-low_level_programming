454cea4d4dae2c0b03a8ddb71de99eab3d051d8d
maqamylee0 <maqamylee0@gmail.com>
2023-06-10 13:54:50 -0700

    add element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	hashnode = malloc(sizeof(hash_node_t));
 	if (hashnode == NULL)
-		return (NULL);
+		return (0);
 	hash = hash_djb2((unsigned char *)key);
 	index = hash % ht->size;
 	hashnode->key = strdup(key);
