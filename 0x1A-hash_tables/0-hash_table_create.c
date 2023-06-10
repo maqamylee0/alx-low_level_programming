8fb8f422dd562330a49669c3f51cea576f880a20
maqamylee0 <maqamylee0@gmail.com>
2023-06-10 11:21:06 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,18 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create- function to crearte hashtable
+ * @size: size of aray
+ * Return: pointer to created hastable
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtab = malloc(sizeof(hash_table_t));
 
-	if (size <= 0)
+	if (hashtab == NULL)
 	{
 		free(hashtab);
-		return NULL;
+		return (NULL);
 	}
 	hashtab->size = size;
 	return (hashtab);
