f475d218d39b18dae99899dba1fde405c7ead0c8
maqamylee0 <maqamylee0@gmail.com>
2023-06-11 10:35:26 -0700

    delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete- delete hash table
+ * @ht: hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *hashnode_pointer, *holder;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		hashnode_pointer = ht->array[i];
+		while (hashnode_pointer != NULL)
+		{
+			holder = hashnode_pointer;
+			hashnode_pointer = hashnode_pointer->next;
+			free(holder->key);
+			free(holder->value);
+			holder->next == NULL;
+			free(holder);
+		}
+		ht->array[i] = NULL;
+	}
+	free(ht->array);
+	free(ht);
+}
